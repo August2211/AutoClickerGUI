fd809a95f963f433a7e5406138fecc4e70b57acd
August2211 <53059031+August2211@users.noreply.github.com>
2023-03-24 21:06:22 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoClickerGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("./AutoCLicker.dll")]
+        static extern void ClickTimes(int times, int delay);
+        [DllImport("./AutoCLicker.dll")]
+        static extern void initAutoClicker();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            initAutoClicker();
+        }
+
+        private void ClickTimes_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                int.TryParse(((TextBox)FindName("Delay")).Text, out int delay);
+                int.TryParse(((TextBox)sender).Text, out int clickTimes);
+                ClickTimes(clickTimes, delay);
+            }
+            catch (Exception easd)
+            {
+            }
+        }
+
+        private void Delay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                int.TryParse(((TextBox)FindName("Click_Times")).Text, out int clickTimes);
+                int.TryParse(((TextBox)sender).Text, out int delay);
+                ClickTimes(clickTimes, delay);
+            }
+            catch (Exception easd)
+            {
+            }
+        }
+    }
+}
